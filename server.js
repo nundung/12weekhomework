ea667bf802160e124452ec484e2b3fd19eaf0b36
nundung <jjy809123@navercom>
2023-12-15 09:27:57 +0000

    restful api로 수정했음

--- server.js
+++ server.js
@@ -2,143 +2,149 @@
     const express = require("express")
 
 
-
 //init
     const app = express()
     const port = 8000
 
 
-
+//post는 form태그, get은 url로 보냄
 //Apis
-
     //회원가입 페이지
-    //프론트엔드가 보내준 값을 저장
-    app.post("/signUp", (req, res) => {
+    app.post("/users", (req, res) => {
         const {id, pw, name, email} = req.body
+        //백엔드에서 프론트로 보내줄 값 미리 생성
+        const signUpResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    //백엔드에서 프론트로 보내줄 값 미리 생성
-    const signUpresult = {
-        "success": false,
-        "message": ""
-    }
 
+    //아이디 중복체크 페이지
+    app.get("/users/id", (req, res) => {
+        const {id} = req.query
+        const checkIdResult = {
+            "success": false,
+            "message": ""
+        }
+    })
 
     //로그인 페이지
-    app.post("/logIn", (req, res) => {
+    app.post("/login", (req, res) => {
         const {id, pw} = req.body
+        const logInResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    const logInResult = {
-        "success": false,
-        "message": ""
-    }
-
 
     //아이디 찾기 페이지
-    app.get("/findId", (req, res) => {
+    app.post("/users/find-id", (req, res) => {
         const {name, email} = req.body
+        const findIdResult = {
+            "success": false,
+            "message": "",
+            "id": null
+        }
     })
-    const findIdResult = {
-        "success": false,
-        "message": ""
-    }
-
 
     //비밀번호 찾기 페이지
-    app.get("/findPw", (req, res) => {
+    app.post("/users/find-pw", (req, res) => {
         const {id, email} = req.body
+        const findPwResult = {
+            "success": false,
+            "message": "",
+            "pw": null
+        }
     })
-    const findPwResult = {
-        "success": false,
-        "message": ""
-    }
-
 
     //내정보 페이지
-
     //내정보 보기
-    app.put("/Info", (req, res) => {
+    app.get("/users/:id/info", (req, res) => {
         const {id, pw, name, email} = req.body
+        const findIdResult = {
+            "success": false,
+            "message": ""
+        }
     })
     //내정보 수정
-    app.put("/Info", (req, res) => {
+    app.put("/users/:id/info", (req, res) => {
         const {pw, name, email} = req.body
+        const editInfoResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    const editInfoResult = {
-        "success": false,
-        "message": ""
-    }
-
-
     //회원탈퇴 페이지
     //계정 삭제
-    app.delete("/deleteAccount", (req, res) => {
-        const {pw} = req.body
+    app.delete("/users/:id", (req, res) => {
+        const {userId} = req.body
+        const deleteResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    const deleteAccountResult = {
-        "success": false,
-        "message": ""
-    }
 
-
-    //게시글 페이지
-    //게시글 읽기,  댓글 읽기
-    app.get("/viewPost", (req, res) => {
-        const {postTitle, postContent, commentContent} = req.body
+    //게시글 목록(게시판) 페이지
+    app.get("/posts", (req, res) => {
+        const {postTitle, postAuthorId} = req.body
     })
-    //게시글 삭제
-    app.delete("/viewPost", (req, res) => {
-        const {pw} = req.body
-    })
-    const deletePostResult = {
-        "success": false,
-        "message": ""
-    }
-    //댓글 쓰기
-    app.post("/viewPost", (req, res) => {
-        const {commentContent} = req.body
-    })
-    const makeCommentResult = {
-        "success": false,
-        "message": ""
-    }
-    //댓글 수정
-    app.put("/viewPost", (req, res) => {
-        const {commentContent} = req.body
-    })
-    const editCommentResult = {
-        "success": false,
-        "message": ""
-    }
-    //댓글 삭제
-    app.delete("/viewPost", (req, res) => {
-        const {commentContent} = req.body
-    })
-    const deleteCommentResult = {
-        "success": false,
-        "message": ""
-    }
-
-
     //게시글 쓰기 페이지
     //게시글 쓰기
-    app.post("/makePost", (req, res) => {
+    app.post("/posts", (req, res) => {
         const {postTitle, postContent} = req.body
+        const makePostResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    const makePostResult = {
-        "success": false,
-        "message": ""
-    }
     //게시글 수정
-    app.put("/makePost", (req, res) => {
-        const {pw,name, email} = req.body
+    app.put("/posts/:postid", (req, res) => {
+        const {postTitle, postContent} = req.body
+        const editPostResult = {
+            "success": false,
+            "message": ""
+        }
     })
-    const editPostResult = {
-        "success": false,
-        "message": ""
-    }
 
+    //게시글 삭제
+    app.delete("/posts/:postid", (req, res) => {
+        const {postId} = req.body
+        const deletePostResult = {
+            "success": false,
+            "message": ""
+        }
+    })
+    
 
+    //게시글 읽기, 댓글 읽기
+    app.get("/posts/:postid", (req, res) => {
+        const {postTitle,postAuthorId, postContent, commentAuthorId, commentContent} = req.body
+    })
 
+    //댓글 쓰기
+    app.post("/posts/:postid/comments", (req, res) => {
+        const {commentContent} = req.body
+        const makeCommentResult = {
+            "success": false,
+            "message": ""
+        }
+    })
+    //댓글 수정
+    app.put("/posts/:postid/comments/:commentid", (req, res) => {
+        const {commentContent} = req.body
+        const editCommentResult = {
+            "success": false,
+            "message": ""
+        }
+    })
+    //댓글 삭제
+    app.delete("/posts/:postid/comments/:commentid", (req, res) => {
+        const {commentId} = req.body
+        const deleteCommentResult = {
+            "success": false,
+            "message": ""
+        }
+    })
 
 
 //web Server
